dad69484262bc50a51c8dfdfad9592d4b08c93d7
Exlo <46317321+Exlo84@users.noreply.github.com>
2019-10-05 23:25:23 +0200

    Update armchainnode.sh

--- armchainnode.sh
+++ armchainnode.sh
@@ -13,7 +13,6 @@ echo 'Installing XERO Node binary'
 echo '**************************'
 
 # Download node binary
-
 wget https://github.com/xero-official/go-xerom/releases/download/2.0.2/geth-arm.zip
 
 unzip geth-arm.zip
